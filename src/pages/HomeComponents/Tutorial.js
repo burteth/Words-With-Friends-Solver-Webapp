095daed3095ed6818680cf32613a8082d6104738
burteth <ethanb127@gmail.com>
2020-08-23 10:46:52 -0400

    Added Mobile Tutorial

--- src/pages/HomeComponents/Tutorial.js
+++ src/pages/HomeComponents/Tutorial.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MAX_PAGES = 4;
+const MAX_PAGES = 7;
 
 export default class Tutorial extends React.Component {
   constructor(props){
@@ -23,18 +23,17 @@ export default class Tutorial extends React.Component {
     }else if (pageNum === 1){
       return(
         <div className="pageBlock">
-          	<div className="pageHeader">How to Play</div>  
+          	<div className="pageHeader">Input Letters</div>  
 
 			<div className="pagePartitionBox">
 
 				<div className="pagePartition" id="pageLeftPartition">
-				<h6>Input Letters:</h6>
-				<h7>
+				<h6>
 					<li>Click on the tile where you would like to input a letter</li>
 					<li>Type the letter that you would like placed</li>
 					<li>Delete letters my typing a space instead of a letter</li>
 					<li>Blank Tiles can be used by typing in a question mark (?)</li>
-				</h7>
+				</h6>
 				</div>
 				<div className="pagePartition" id="pageRightPartition">
 					<img id="pageGif"src="https://media.giphy.com/media/IgXWLeNcEuQPBr6VL1/giphy.gif"/>
@@ -43,27 +42,104 @@ export default class Tutorial extends React.Component {
         </div>
       )
     }else if (pageNum === 2){
-      return(
-        <div className="pageBlock">
-          	<div className="pageHeader">How to Play</div>  
-
-			<div className="pagePartitionBox">
-
-				<div className="pagePartition" id="pageLeftPartition">
-				<h6>Finding Solutions:</h6>
-				<h7>
-					<li>Click on the find solutions button under the board</li>
-					<li>Navigate through the solutions using the arrow buttons</li>
-					<li>Automatically input a suggested word by clicking on it</li>
-				</h7>
-				</div>
-				<div className="pagePartition" id="pageRightPartition">
-					<img id="pageGif" src="https://media.giphy.com/media/Wm8r2Ye1kiKe67EDm6/giphy.gif"/>
-				</div>
-			</div>
-        </div>
-      )
-    }
+		return(
+		  <div className="pageBlock">
+				<div className="pageHeader">Deleting Letters</div>  
+  
+			  <div className="pagePartitionBox">
+  
+				  <div className="pagePartition" id="pageLeftPartition">
+				  	<h6>
+					  <li>You can remove a letter by typing a space instead of a letter</li>
+					</h6>
+				  </div>
+				  <div className="pagePartition" id="pageRightPartition">
+					  <img id="pageGif" src="https://media.giphy.com/media/Wm8r2Ye1kiKe67EDm6/giphy.gif"/>
+				  </div>
+			  </div>
+		  </div>
+		)
+	  }else if (pageNum === 3){
+		return(
+		  <div className="pageBlock">
+				<div className="pageHeader">Blank Tiles</div>  
+  
+			  <div className="pagePartitionBox">
+  
+				  <div className="pagePartition" id="pageLeftPartition">
+				  <h6>
+					  <li>Blank Tiles can be input by typing a question mark</li>
+				  </h6>
+				  </div>
+				  <div className="pagePartition" id="pageRightPartition">
+					  <img id="pageGif" src="https://media.giphy.com/media/Wm8r2Ye1kiKe67EDm6/giphy.gif"/>
+				  </div>
+			  </div>
+		  </div>
+		)
+	  }else if (pageNum === 4){
+		return(
+		  <div className="pageBlock">
+				<div className="pageHeader">Finding Solutions</div>  
+  
+			  <div className="pagePartitionBox">
+  
+				  <div className="pagePartition" id="pageLeftPartition">
+				  <h6>
+					  <li>Click on the find solutions button under the board</li>
+					  <li>Navigate through the solutions using the arrow buttons</li>
+					  <li>Automatically input a suggested word by clicking on it</li>
+				  </h6>
+				  </div>
+				  <div className="pagePartition" id="pageRightPartition">
+					  <img id="pageGif" src="https://media.giphy.com/media/Wm8r2Ye1kiKe67EDm6/giphy.gif"/>
+				  </div>
+			  </div>
+		  </div>
+		)
+	  }else if (pageNum === 5){
+		return(
+		  <div className="pageBlock">
+				<div className="pageHeader">How to Play</div>  
+  
+			  <div className="pagePartitionBox">
+  
+				  <div className="pagePartition" id="pageLeftPartition">
+				  <h6>Finding Solutions:</h6>
+				  <h6>
+					  <li>Click on the find solutions button under the board</li>
+					  <li>Navigate through the solutions using the arrow buttons</li>
+					  <li>Automatically input a suggested word by clicking on it</li>
+				  </h6>
+				  </div>
+				  <div className="pagePartition" id="pageRightPartition">
+					  <img id="pageGif" src="https://media.giphy.com/media/Wm8r2Ye1kiKe67EDm6/giphy.gif"/>
+				  </div>
+			  </div>
+		  </div>
+		)
+	  }else if (pageNum === 6){
+		return(
+		  <div className="pageBlock">
+				<div className="pageHeader">How to Play</div>  
+  
+			  <div className="pagePartitionBox">
+  
+				  <div className="pagePartition" id="pageLeftPartition">
+				  <h6>Finding Solutions:</h6>
+				  <h6>
+					  <li>Click on the find solutions button under the board</li>
+					  <li>Navigate through the solutions using the arrow buttons</li>
+					  <li>Automatically input a suggested word by clicking on it</li>
+				  </h6>
+				  </div>
+				  <div className="pagePartition" id="pageRightPartition">
+					  <img id="pageGif" src="https://media.giphy.com/media/Wm8r2Ye1kiKe67EDm6/giphy.gif"/>
+				  </div>
+			  </div>
+		  </div>
+		)
+	  }
 
   }
 
